64784920cf2ecd9dce9de64bd9de39b8ca1bc346
Melvin Mathew <36991926+melvingm101@users.noreply.github.com>
2023-11-01 14:12:36 +0400

    Creating project and adding full screen capabilities

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { onMount } from "svelte";
+  import { appWindow } from "@tauri-apps/api/window";
+  import { onDestroy } from "svelte";
+
+  const fullScreenChange = async () => {
+    if (document.fullscreenElement) {
+      await appWindow.setFullscreen(true);
+    } else {
+      await appWindow.setFullscreen(false);
+    }
+  };
+
+  onMount(() => {
+    document.addEventListener("fullscreenchange", fullScreenChange);
+  });
+
+  onDestroy(() => {
+    document.removeEventListener("fullscreenchange", fullScreenChange);
+  });
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
